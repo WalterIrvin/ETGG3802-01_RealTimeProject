0cd539ee30f3c8a21c8cce7d10ea7c11603ffc39
Walter Irvin <43546119+WalterIrvin@users.noreply.github.com>
2019-02-19 13:23:17 -0500

    added health
    Health didn't get added for some reason last commit, so this adds the health to the enemy

--- Assets/Scripts/tmp_EnemyMover.cs
+++ Assets/Scripts/tmp_EnemyMover.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class tmp_EnemyMover : MonoBehaviour
 {
     public GameObject tmp;
+    public float health = 100;
     // Start is called before the first frame update
     void Start()
     {
