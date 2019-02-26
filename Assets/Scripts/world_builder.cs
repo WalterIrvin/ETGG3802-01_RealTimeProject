b4d75012ecafcbe26b0bc9505178aed4a8d5ff00
cheapscate98 <35314966+cheapscate98@users.noreply.github.com>
2019-02-26 13:52:30 -0500

    Branch Base
    Added functionality for construction/destruction
    Known Bugs:
    -enemies might stop and think about new paths for a few seconds
    -You can completely block the path by lacing blocks
    -The code that is supposed to prevent you from blocking paths can break and prevent you from placing any blocks at all
    -The code for placing blocks may not place a block where you want it.

--- Assets/Scripts/world_builder.cs
+++ Assets/Scripts/world_builder.cs
@@ -8,12 +8,103 @@ public class world_builder : MonoBehaviour
     [SerializeField]
     NavMeshSurface[] navMeshSurfaces;
 
+    public Transform Obstacle;
+
     // Start is called before the first frame update
     void Start()
     {
         for (int i = 0; i < navMeshSurfaces.Length; i++)
         {
             navMeshSurfaces[i].BuildNavMesh();
+        }
+    }
+
+    void Update()
+    {
+
+        // If the mouse button is down, add a block if possible
+        if (Input.GetMouseButtonDown(0))
+        {
+            Camera cam = (Camera)GameObject.Find("Main Camera").GetComponent(typeof(Camera));
+
+            Ray from_camera = cam.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit result;
+
+            //If we hit the world, do stuff
+            if (Physics.Raycast(from_camera, out result))
+            {
+                if (result.collider.tag == "World")
+                {
+
+                    // Snapping to grid
+                    // this uses a cheap rounding method I found while screwing around.
+                    // The number I get out of this is right, so the issue seems to be
+                    // caused by incorrect input data (most likely user error...)
+                    int create_x = (int)(result.point.x + .5);
+                    int create_z = (int)(result.point.z + .5);
+                    create_z -= 1;
+
+
+
+                    // Creating the new wall and baking the navmesh around it
+                    Transform newbie = Instantiate(Obstacle, new Vector3(create_x, 0, create_z), Quaternion.identity);
+
+
+
+                    // Check to see if all enemies still have a path to the exit
+                    GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+                    foreach (GameObject enemy in enemies)
+                    {
+                        NavMeshAgent tmp = (NavMeshAgent)enemy.GetComponent(typeof(NavMeshAgent));
+
+                        // Checking to see if the object has a NavMeshAgent (if not then there is a problem...)
+                        if (tmp != null)
+                        {
+                            //Calculating the path the object needs to take since it may not have recalculated after the navmesh was rebaked
+                            tmp.CalculatePath(tmp.destination, tmp.path);
+                            //Checking to see if the enemy has a path to the exit
+                            if (tmp.path.status == NavMeshPathStatus.PathPartial)
+                            {
+                                Debug.Log("Bad Obstacle Placement");
+
+                                Destroy(newbie.gameObject);
+
+                                return;
+
+                            }
+
+
+
+                        }
+
+
+
+                    }
+                }
+            }
+
+
+        }
+
+
+
+        // Right Click to destroy walls (not actually a feature, just here for testing)
+        if (Input.GetMouseButtonDown(1))
+        {
+            Camera cam = (Camera)GameObject.Find("Main Camera").GetComponent(typeof(Camera));
+
+            Ray from_camera = cam.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit result;
+
+            if (Physics.Raycast(from_camera, out result))
+            {
+                if (result.collider.tag == "Wall")
+                {
+                    Destroy(result.collider.gameObject);
+                }
+            }
         }
     }
 }
