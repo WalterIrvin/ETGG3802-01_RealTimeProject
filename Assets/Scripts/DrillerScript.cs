e269561f3003c59409de53307fff25cf58dcae64
Jonathan Lawrence <lawrencej2@mymail.shawnee.edu>
2019-03-01 01:29:11 -0500

    Updated Driller to now only break destrcutible objects
    - Removed WallScript as it no longer has a use
    - Added Destructible tag for designating which blocks a driller will target
    - Included ability to toggle whether or not a driller dies when it breaks a block
    - Updated Level1 to support the new driller

--- Assets/Scripts/DrillerScript.cs
+++ Assets/Scripts/DrillerScript.cs
@@ -1,15 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class DrillerScript : MonoBehaviour
 {
+    public int value = 100;
+    public float agentAcceleration = 1;
+    public float agentMaxSpeed = 1;
     public float health = 100;
+    private float maxHealth;
+    public bool isFragile = true; // set to false if you want the driller to not die when it breaks a destructible.
+    private GameObject MoneyHandle;
+
+    [SerializeField]
+    List<Transform> _destinations = new List<Transform>();
+
+    NavMeshAgent _navMeshAgent;
 
     void Start()
     {
+        maxHealth = health;
 
+        _navMeshAgent = this.GetComponent<NavMeshAgent>();
+
+        if (_navMeshAgent == null)
+        {
+            Debug.LogError("The navmesh agent component is not attached to " + gameObject.name);
+        }
+        else
+        {
+            InitializeDestination();
+        }
+        _navMeshAgent.speed = agentMaxSpeed;
+        _navMeshAgent.acceleration = agentAcceleration;
+        MoneyHandle = GameObject.FindWithTag("Money");
     }
 
     void Update()
@@ -17,9 +43,61 @@ public class DrillerScript : MonoBehaviour
         if (this.health <= 0)
         {
             Destroy(gameObject);
-            return;
+            MoneyHandle.BroadcastMessage("ChangeMoney", value);
         }
-        transform.position += transform.forward * Time.deltaTime;
+
+        UpdateDestination();
     }
 
+    private void InitializeDestination()
+    {
+        foreach (GameObject tmpObj in GameObject.FindGameObjectsWithTag("Destructible"))
+            _destinations.Add(tmpObj.transform);
+
+        foreach (GameObject tmpObj in GameObject.FindGameObjectsWithTag("Base"))
+            _destinations.Add(tmpObj.transform);
+
+        //_destinations = _destinations.OrderBy(x => Vector3.Distance(gameObject.transform.position, x.position)).ToList();
+
+        _destinations.Sort(delegate (Transform a, Transform b)
+        {
+            return Vector3.Distance(this.transform.position, b.position)
+            .CompareTo(
+              Vector3.Distance(this.transform.position, a.position));
+        });
+
+        _destinations.Reverse();
+
+        UpdateDestination();
+    }
+
+    private void UpdateDestination()
+    {
+        if (!_destinations.Any())
+        {
+            Debug.LogError("No Destination for " + gameObject.name);
+            return;
+        }
+
+        while (_destinations[0] == null)
+            _destinations.RemoveAt(0);
+
+        _navMeshAgent.SetDestination(_destinations[0].position);
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Destructible")
+        {
+            Destroy(col.gameObject);
+
+            if (this.isFragile)
+            {
+                Destroy(gameObject);
+                return;
+            }
+        }
+
+        UpdateDestination();
+    }
 }
