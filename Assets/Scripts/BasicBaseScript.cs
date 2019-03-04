f3599bc09cc74468548c64d92b7e9be8e4f2d47b
Jonathan Lawrence <lawrencej2@mymail.shawnee.edu>
2019-03-04 14:27:29 -0500

    Updated Base Health

--- Assets/Scripts/BasicBaseScript.cs
+++ Assets/Scripts/BasicBaseScript.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 public class BasicBaseScript : MonoBehaviour
 {
 
-    public int health;
+    public int health = 100;
 
     // Start is called before the first frame update
     void Start()
     {
-        this.health = 100;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (this.health is 0)
+        if (this.health <= 0)
         {
             Destroy(gameObject);
         }
