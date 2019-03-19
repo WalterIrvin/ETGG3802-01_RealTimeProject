d1fcea402511bba88ec4ace1ca9acafcdaa3fb17
Rachel Roar <rachel_roar@outlook.com>
2019-03-19 12:44:55 -0400

    manager changes

---
+++ assets/scripts/uiscrip/uiManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class uiManager : MonoBehaviour
+{
+    GameObject[] pausedItems;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+        pausedItems = GameObject.FindGameObjectsWithTag("ShowOnPause");
+        hidePaused();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        showPaused();
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            //showPaused();
+            if(Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+                showPaused();
+            } else if(Time.timeScale == 0)
+            {
+                Time.timeScale = 1;
+                hidePaused();
+            }
+        }
+    }
+
+    public void pauseControl()
+    {
+        showPaused();
+        if (Time.timeScale == 1)
+        {
+            Time.timeScale = 0;
+            showPaused();
+        }
+        else if (Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+            hidePaused();
+        }
+    }
+
+    void showPaused()
+    {
+        foreach (GameObject g in pausedItems)
+        {
+            g.SetActive(true);
+        }
+    }
+
+    void hidePaused()
+    {
+        foreach(GameObject g in pausedItems)
+        {
+            g.SetActive(false);
+        }
+    }
+
+    public void loadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
