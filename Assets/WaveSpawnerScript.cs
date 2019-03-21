11bd72ebb110e1223279464278123771292aa14a
Walter Irvin <43546119+WalterIrvin@users.noreply.github.com>
2019-03-20 21:51:47 -0400

    Added wave system
    Added simplistic wave system which scales up per wave, with possibilities to add new enemy types per wave and to scale number of enemies by a scale factor.
    the titan was also nerfed somewhat.

---
+++ Assets/WaveSpawnerScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawnerScript : MonoBehaviour
+{
+    public List<EnemySpawnerScript.mookTier> mookList;
+    public List<EnemySpawnerScript.bossTier> bossList;
+    public int scalingFactor;
+    private int curWave = 1;
+
+    public void addNewMookType(EnemySpawnerScript.mookTier newMook)
+    {
+        mookList.Add(newMook);
+    }
+
+    public void addNewBossType(EnemySpawnerScript.bossTier newBoss)
+    {
+        bossList.Add(newBoss);
+    }
+
+    public void newWave()
+    {
+        foreach(EnemySpawnerScript.mookTier mook in mookList)
+        {
+            mook.amount += scalingFactor * curWave;
+        }
+        foreach(EnemySpawnerScript.bossTier boss in bossList)
+        {
+            boss.amount += scalingFactor * curWave;
+        }
+    }
+
+    void Update()
+    {
+        if(curWave >= 10)
+        {
+            Debug.Log("You Win, Here's a cookie!");
+        }
+    }
+}
