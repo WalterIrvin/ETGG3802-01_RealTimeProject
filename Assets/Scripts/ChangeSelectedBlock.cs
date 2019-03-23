e07378d575b4f5cb554b0282ea20f6b1045daa6b
EstradaAlex20 <estrada.alex20@gmail.com>
2019-03-23 12:26:00 -0400

    forgot to add a script, oopssss

---
+++ Assets/Scripts/ChangeSelectedBlock.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSelectedBlock : MonoBehaviour
+{
+    public GameObject currentlySelectedBlock;
+    public Material selectedMat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentlySelectedBlock.GetComponent<Renderer>().material.color = selectedMat.color;
+
+    }
+}
