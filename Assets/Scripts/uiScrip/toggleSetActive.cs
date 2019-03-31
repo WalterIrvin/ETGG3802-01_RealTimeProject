045f428aad80231e0fdfaa176cfeb9d80382cd21
Walter Irvin <43546119+WalterIrvin@users.noreply.github.com>
2019-03-31 02:54:39 -0400

    Revert "Revert "Merge branch 'UI-edited'""
    This reverts commit 68667096fa41ad6b7c3efa1513aaf64207d5934c.

---
+++ Assets/Scripts/uiScrip/toggleSetActive.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toggleSetActive : MonoBehaviour
+{
+    public GameObject toggler;
+    public bool startActive = false;
+
+    public void toggleGameObject()
+    {
+        startActive = !startActive;
+        toggler.SetActive(startActive);
+    }
+}
