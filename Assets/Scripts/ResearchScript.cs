3c6ca34064627899eb285ce168df5ddbd31ca090
Walter Irvin <43546119+WalterIrvin@users.noreply.github.com>
2019-04-18 14:09:22 -0400

    adding functionality to the research tab
    -added new script for research tab and for each of the buttons.
    -still working on adding functionality to startResearch()

---
+++ Assets/Scripts/ResearchScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class prependStatsBar
+{
+   // public string[];
+}
+public class ResearchScript : MonoBehaviour
+{
+    public TowerDictionary towerDict;
+    public Text flavorText;
+    public Text statsText;
+    void Start()
+    {
+
+    }
+
+    public void startResearch(TowerButtonResearchTab buttonObj)
+    {
+        TowerData tower = buttonObj.attachedTower;
+    }
+
+    void Update()
+    {
+        
+    }
+}
