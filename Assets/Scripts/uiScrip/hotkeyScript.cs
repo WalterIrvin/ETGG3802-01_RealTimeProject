850573366fb1c8dd7b443ea9cfb83615bd6627a0
cheapscate98 <35314966+cheapscate98@users.noreply.github.com>
2019-04-25 13:10:45 -0400

    HOTKEYS!
    Escape pulls up the file menu
    The menu bar is tied to the number row.

---
+++ Assets/Scripts/uiScrip/hotkeyScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class hotkeyScript : MonoBehaviour
+{
+    public Button fileButton;
+    public Button buildMenu;
+    public Button researchMenu;
+    public Button settingsMenu;
+    public Button pauseButton;
+    public Button nextWaveButton;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Pause menu (that doesn't pause...)
+        if (Input.GetKeyDown(KeyCode.Escape)) { fileButton.onClick.Invoke(); }
+
+        //Open the build menu
+        if (Input.GetKeyDown(KeyCode.Alpha1)) { buildMenu.onClick.Invoke(); }
+
+        //Open the research menu
+        if (Input.GetKeyDown(KeyCode.Alpha2)) { researchMenu.onClick.Invoke(); }
+
+        //Open the settings menu
+        if (Input.GetKeyDown(KeyCode.Alpha3)) { settingsMenu.onClick.Invoke(); }
+
+        //Pause the game
+        if (Input.GetKeyDown(KeyCode.Alpha4)) { pauseButton.onClick.Invoke(); }
+
+        //Skip downtime and go to the next wave
+        if (Input.GetKeyDown(KeyCode.Alpha5)) { nextWaveButton.onClick.Invoke(); }
+    }
+}
