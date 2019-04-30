3afb6d972d0ff08a6540e733b9939d9168bd7a9b
Michael Hill <35314966+cheapscate98@users.noreply.github.com>
2019-04-29 23:30:26 -0400

    In game menu functionality
    Added the ability to return to the main menu and quit the game from the in game file menu.

---
+++ Assets/Scripts/uiScrip/FileMenuScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FileMenuScript : MonoBehaviour
+{
+    public void newGame()
+    {
+        SceneManager.LoadScene("menuScreen");
+    }
+
+    public void quitGame()
+    {
+        Debug.Log("If you are running this in the editor, nothing will happen.");
+        Application.Quit();
+    }
+}
