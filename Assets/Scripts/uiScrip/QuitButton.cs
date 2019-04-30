975c8de1bb2c75d1c55f583b7f59189773acdea3
Michael Hill <35314966+cheapscate98@users.noreply.github.com>
2019-04-29 23:14:54 -0400

    Quit Button
    Quit button on the main menu now actually quits the game (except when run in the editor)

---
+++ Assets/Scripts/uiScrip/QuitButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitButton : MonoBehaviour
+{
+    public void quitGame()
+    {
+        Debug.Log("If you are in the editor, the game won't actually quit...");
+        Application.Quit();
+    }
+}
